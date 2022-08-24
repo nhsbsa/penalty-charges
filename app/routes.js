63f9c354254db257b54f36b7a2b79d1b57cea0db
cbroa <charlotte.broad1@nhs.net>
2022-08-24 13:22:47 +0100

    pcn and decs enquiry

--- app/routes.js
+++ app/routes.js
@@ -538,7 +538,7 @@ router.get(/dwp-new-handler/, function (req, res) {
           res.redirect('dwp-check-your-answers');
         }
          else if (req.query.detailsaddress == 'no') {
-         res.redirect('dwp-update-address');;
+         res.redirect('dwp-update-address-uc-pecs');;
          }
        });
 
@@ -573,6 +573,113 @@ router.get(/postcode-handler/, function (req, res) {
   else {
     res.redirect('unable-to-confirm-id');
   }
+});  
+
+
+
+
+
+
+// DECS JOURNEY
+
+router.get(/taxcredit-handler-copy/, function (req, res) {
+  if (req.query.taxcredit == 'yes') {
+    res.redirect('cert-number');
+  }
+   else if (req.query.taxcredit == 'no') {
+   res.redirect('dwp-did-you-bsa');;
+   }
+ });
+ router.get(/dwpclaiming-isa-copy/, function (req, res) {
+  if (req.query.exemptionbenefit == 'yes') {
+    res.redirect('dwp-check-name-uc');
+  }
+   else if (req.query.exemptionbenefit == 'no') {
+   res.redirect('dwp-did-you-bsa');;
+   }
+ });
+ router.get(/bsa-handler-copy/, function (req, res) {
+  if (req.query.bsa == 'yes') {
+    res.redirect('dwp-check-name-uc');
+  }
+  else if (req.query.bsa == 'mat') {
+    res.redirect('dwp-check-name-uc');
+  }
+  else if (req.query.bsa == 'ppc') {
+    res.redirect('dwp-check-name-uc');
+  }
+   else if (req.query.bsa == 'med') {
+     res.redirect('dwp-check-name-uc');
+   }
+   else if (req.query.bsa== 'hc2') {
+     res.redirect('dwp-check-name-uc');
+   }
+   else if (req.query.bsa == 'tc') {
+     res.redirect('dwp-check-name-uc');
+   }
+   else if (req.query.bsa == 'no') {
+     res.redirect('dwp-exemptions-decs');
+   }
+ });
+ router.get(/dwpexemptions-handler-copy/, function (req, res) {
+  if (req.query.benefitsdwp == 'yes') {
+    res.redirect('dwp-check-name-uc');
+  }
+  else if (req.query.benefitsdwp == 'is') {
+    res.redirect('dwp-check-name-uc');
+  }
+  else if (req.query.benefitsdwp == 'jsa') {
+    res.redirect('dwp-check-name-uc');
+  }
+   else if (req.query.benefitsdwp == 'uc') {
+     res.redirect('dwp-check-name-uc');
+   }
+   else if (req.query.benefitsdwp == 'pc') {
+     res.redirect('dwp-check-name-uc');
+   }
+  
+   else if (req.query.benefitsdwp == 'no') {
+     res.redirect('dwp-we-need-proof-decs');
+   }
+ });
+ router.get(/updatename-copy/, function (req, res) {
+  if (req.query.details == 'yes') {
+    res.redirect('dwp-check-dob');
+  }
+   else if (req.query.details == 'no') {
+   res.redirect('dwp-update-name');;
+   }
+ });
+
+ router.get(/checkdob-copy/, function (req, res) {
+  if (req.query.detailsdob == 'yes') {
+    res.redirect('dwp-check-address-decs');
+  }
+   else if (req.query.detailsdob == 'no') {
+   res.redirect('dwp-update-dob');;
+   }
+ });
+
+ router.get(/checkaddress-copy/, function (req, res) {
+  if (req.query.detailsaddress == 'yes') {
+    res.redirect('dwp-check-your-answers');
+  }
+   else if (req.query.detailsaddress == 'no') {
+   res.redirect('dwp-update-address-uc-decs');;
+   }
+ });
+
+
+// Postcode route
+router.get(/postcodehandler-copy/, function (req, res) {
+if (req.query.postcode == 'NE1 3JA') {
+res.redirect('view-dwp-challenge-decs');
+} else if (req.query.postcode == 'NE2 4XL') {
+res.redirect('view-dwp-challenge-decs-copy');;
+}
+else {
+res.redirect('unable-to-confirm-id');
+}
 });  
 
 module.exports = router;
